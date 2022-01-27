d17b9f4f2a1cb48b777e9d2dcd9c4c2965a0e565
Gialicus <51245135+Gialicus@users.noreply.github.com>
2022-01-27 17:30:13 +0100

    Update csvToJson.go

--- cmd/csvToJson.go
+++ cmd/csvToJson.go
@@ -21,13 +21,8 @@ import (
 // csvToJsonCmd represents the csvToJson command
 var csvToJsonCmd = &cobra.Command{
 	Use:   "csvToJson",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "csv to json writer",
+	Long:  `trasforma csv in json`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("csvToJson called")
 		csvToJson(cmd, args)
